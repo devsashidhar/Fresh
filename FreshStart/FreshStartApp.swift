ddb4083ba3f231d2ff093c3f032db4962d8ca64d
Dev Sashidhar <devsashidhar@gmail.com>
2024-10-06 18:20:58 -0400

    Initial Commit

---
+++ FreshStart/FreshStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshStartApp.swift
+//  FreshStart
+//
+//  Created by Dev Sashidhar on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FreshStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
